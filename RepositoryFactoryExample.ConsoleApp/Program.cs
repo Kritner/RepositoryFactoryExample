a2f1a6d0a62153c907ed0d7ca419e8e66783a58d
kritner <kritner@gmail.com>
2015-12-30 07:20:48 -0500

    Project creation

---
+++ RepositoryFactoryExample.ConsoleApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepositoryFactoryExample.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Repository Factory Example \n\n");
+
+
+        }
+    }
+}
