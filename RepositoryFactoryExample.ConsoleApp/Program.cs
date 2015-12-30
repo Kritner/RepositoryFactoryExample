4bd92565a4e9e98df20f0a92e325619cac20c740
kritner <kritner@gmail.com>
2015-12-30 07:33:04 -0500

    Create two repositories of differing types based on a form type.

--- RepositoryFactoryExample.ConsoleApp/Program.cs
+++ RepositoryFactoryExample.ConsoleApp/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using RepositoryFactoryExample.Business.Enum;
+using RepositoryFactoryExample.Business.Factory;
 
 namespace RepositoryFactoryExample.ConsoleApp
 {
@@ -12,7 +14,13 @@ namespace RepositoryFactoryExample.ConsoleApp
         {
             Console.WriteLine("Repository Factory Example \n\n");
 
+            RepositoryFactory factory = new RepositoryFactory();
 
+            var repo9c2 = factory.GetRepository(FormTypes.Form64_9C2);
+            repo9c2.SayYourName();
+
+            var repo9base = factory.GetRepository(FormTypes.Form64_9Base);
+            repo9base.SayYourName();
         }
     }
 }
